4ff8fdb1ef1c71b2fca8f03d345b69824b61c236
Anh Do <doquocanh.msu@gmail.com>
2020-02-07 23:03:39 -0600

    Simplify field empty checking

--- app/server/routes/api/users.js
+++ app/server/routes/api/users.js
@@ -14,8 +14,7 @@ const User = require('../../models/Users');
 // @access  Public
 router.post('/', [
         check('name', "Name is required")
-        .not()
-        .isEmpty(),
+        .notEmpty(),
         check('email', 'Please include a valid email address').isEmail(),
         check('password', 'Please enter a password with 6 or more characters')
         .isLength({
